ed0e941e146804ef29cf6f4b6f16db3aaea287d6
Jacob Malone <jakemalone11@gmail.com>
2020-05-13 16:20:06 -0500

    Simplify discovery.

--- app/src/main/java/xyz/mcmxciv/halauncher/ui/setup/discovery/ServiceAdapter.kt
+++ app/src/main/java/xyz/mcmxciv/halauncher/ui/setup/discovery/ServiceAdapter.kt
@@ -2,15 +2,21 @@ package xyz.mcmxciv.halauncher.ui.setup.discovery
 
 import android.net.nsd.NsdServiceInfo
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import xyz.mcmxciv.halauncher.databinding.ListItemServiceBinding
 
-class ServiceAdapter(
-    private val listener: ServiceSelectedListener
-) : RecyclerView.Adapter<ServiceAdapter.ServiceListViewHolder>() {
-
+class ServiceAdapter : RecyclerView.Adapter<ServiceAdapter.ServiceListViewHolder>() {
     private var serviceList: List<NsdServiceInfo> = ArrayList()
+    private lateinit var onServiceSelectedListener: (url: String) -> Unit
+
+    var data: List<NsdServiceInfo>
+        get() = serviceList
+        set(value) {
+            serviceList = value
+            notifyDataSetChanged()
+        }
 
     class ServiceListViewHolder(val binding: ListItemServiceBinding)
         : RecyclerView.ViewHolder(binding.root)
@@ -29,15 +35,14 @@ class ServiceAdapter(
 
         holder.binding.serviceNameText.text = name
         holder.binding.serviceHostUrl.text = url
-        holder.binding.serviceItemContainer.setOnClickListener {
-            listener.onServiceSelected(url)
+        holder.binding.root.setOnClickListener {
+            onServiceSelectedListener(url)
         }
     }
 
     override fun getItemCount(): Int = serviceList.count()
 
-    fun setData(services: List<NsdServiceInfo>) {
-        serviceList = services
-        notifyDataSetChanged()
+    fun setOnServiceSelectedListener(l: (url: String) -> Unit) {
+        onServiceSelectedListener = l
     }
 }
